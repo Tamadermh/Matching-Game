67f07bef182edade2020f58a724462c9a66f5141
Tamader <tmadr246@gmail.com>
2022-03-04 03:56:13 +0300

    edit variable

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// varible
+const heart = document.querySelectorAll("#heart li");
+const moves = document.querySelector("#moves");
+const timer = document.querySelector("#timer");
+const restart = document.querySelector("#restart"); 
+const cardToshuffle = document.querySelectorAll("#dack li");
+let arr = Array.from(cardToshuffle);
+let openCards =[];
+let movesCounter =0;
+let timerOut=true ;
+let match =0;
+let time =0;
+//functions
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -11,25 +24,7 @@ function shuffle(array) {
 
     return array;
 }
-
-// varible
-const dack = document.querySelector("#deck");
-const heart = document.querySelectorAll("#heart li");
-const moves = document.querySelector("#moves");
-const timer = document.querySelector("#timer");
-const restart = document.querySelector("#restart"); 
-const card = document.querySelectorAll("#dack li");
-let arr = Array.from(cardToShuffle);
-let openCards =[];
-let movesCounter =0;
-let timerOut=true ;
-let match =0;
-let time =0;
-
-//functions
-
-
-
-
-
 // event listeners
+
+
+
